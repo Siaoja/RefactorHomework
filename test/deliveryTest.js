d7e725fbcae07989537507be82055d3d0ce71c23
Siaoja <lxj971010>
2020-08-28 12:35:37 +0800

    [refactor homework][spike]feature: add test deliveryDate given anOrder with MA and isRush with true return 2.

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -1,8 +1,16 @@
 const deliveryTest = require('ava');
 const {deliveryDate} = require("../src/delivery");
 
-deliveryTest('bar', async t => {
-    const bar = Promise.resolve('bar');
-    t.is(await bar, 'bar');
+deliveryTest('deliveryDate given anOrder with MA and isRush with true return 2', t => {
+    let isRush = true;
+    let anOrder = {
+        deliveryState: 'MA',
+        placedOn: {
+            plusDays: (plusDeliveryTime) => {
+                return plusDeliveryTime;
+            }
+        }
+    };
+    let result = deliveryDate(anOrder, isRush);
+    t.is(result, 2);
 });
-
