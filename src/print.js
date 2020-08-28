3dbadd679f52e3acd138965194ee89fb1758794c
Siaoja <lxj971010>
2020-08-28 19:04:39 +0800

    [refactor homework][spike]refactor: exact method calculateTotalOutstanding, recordDueDate, printDetail, generateDetail.

--- src/print.js
+++ src/print.js
@@ -1,26 +1,43 @@
-function printOwing (invoice) {
+function calculateTotalOutstanding(invoice) {
   let outstanding = 0;
-  let result = '***********************\n**** Customer Owes ****\n***********************\n';
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
-
-  // calculate outstanding
   for (const o of invoice.borderSpacing) {
     outstanding += o.amount;
   }
+  return outstanding;
+}
 
-  // record due date
+function recordDueDate(invoice) {
   const today = new Date();
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
 
-  // print details
+function printDetail(invoice, outstanding) {
+  console.log('***********************');
+  console.log('**** Customer Owes ****');
+  console.log('***********************');
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
   console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
+function generateDetail(invoice, outstanding) {
+  let result = '***********************\n**** Customer Owes ****\n***********************\n';
   result += `name: ${invoice.customer}\n`;
   result += `amount: ${outstanding}\n`;
   result += `amount: ${invoice.dueDate.toLocaleDateString()}`;
+  return result;
+}
+
+function printOwing (invoice) {
+  let outstanding = 0;
+  let result;
+
+  outstanding = calculateTotalOutstanding(invoice);
+
+  recordDueDate(invoice);
+
+  printDetail(invoice, outstanding);
+  result = generateDetail(invoice, outstanding);
   return result;
 }
 module.exports ={
