e11b0d198ce4bb7591bc7efc979abce453446775
Siaoja <lxj971010>
2020-08-28 12:37:05 +0800

    [refactor homework][spike]feature: add test deliveryDate given anOrder with NY and isRush with true return 3.

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -14,3 +14,19 @@ deliveryTest('deliveryDate given anOrder with MA and isRush with true return 2',
     let result = deliveryDate(anOrder, isRush);
     t.is(result, 2);
 });
+
+
+deliveryTest('deliveryDate given anOrder with NY and isRush with true return 3', t => {
+    let isRush = true;
+    let anOrder = {
+        deliveryState: 'NY',
+        placedOn: {
+            plusDays: (plusDeliveryTime) => {
+                return plusDeliveryTime;
+            }
+        }
+    };
+
+    let result = deliveryDate(anOrder, isRush);
+    t.is(result, 3);
+})
