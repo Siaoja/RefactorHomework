2c63d2b161ab1aa8b88baf6a2507d1c7dc0d327a
Siaoja <lxj971010>
2020-08-28 18:59:51 +0800

    [refactor homework][spike]feature: add test method given invoice with customer and borderSpacing when printOwing return data result.

--- src/print.js
+++ src/print.js
@@ -1,5 +1,6 @@
 function printOwing (invoice) {
   let outstanding = 0;
+  let result = '***********************\n**** Customer Owes ****\n***********************\n';
   console.log('***********************');
   console.log('**** Customer Owes ****');
   console.log('***********************');
@@ -17,4 +18,11 @@ function printOwing (invoice) {
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
   console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
+  result += `name: ${invoice.customer}\n`;
+  result += `amount: ${outstanding}\n`;
+  result += `amount: ${invoice.dueDate.toLocaleDateString()}`;
+  return result;
+}
+module.exports ={
+  printOwing,
 }
--- test/printTest.js
+++ test/printTest.js
@@ -1,21 +1,25 @@
 const printTest = require('ava');
+const {printOwing} = require("../src/print");
 
 
-printTest('foo', t => {
-    t.pass();
+printTest('given invoice with customer and borderSpacing when printOwing return data result', t => {
+    const invoice = {
+        "customer": "spike",
+        "borderSpacing": [
+            {amount: 10},
+            {amount: 20}
+        ]
+    };
+    let today = new Date();
+    let plusDay = new Date();
+    plusDay.setDate(today.getDate() + 30)
+
+
+    const result = printOwing(invoice);
+    t.is(result, '***********************\n' +
+        '**** Customer Owes ****\n' +
+        '***********************\n' +
+        'name: spike\n'+
+        'amount: 30\n' +
+        'amount: '+plusDay.toLocaleDateString())
 });
-
-printTest('bar', async t => {
-    const bar = Promise.resolve('bar');
-    t.is(await bar, 'bar');
-});
-//
-// printTest('pintOwing function test', t => {
-//     let invoice = {
-//         'borderSpacing':[],
-//         'dueDate':{},
-//     }
-//     invoice.customer = 'spike';
-//     invoice.borderSpacing = [{'amount': 4},{'amount': 5},{'amount': 6},{'amount': 7}];
-//
-// });
